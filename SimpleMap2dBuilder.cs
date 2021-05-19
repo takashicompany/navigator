b7af511dd58919c4263dd307458f191f7422b219
takashicompany <t@kashi.company>
2021-05-19 18:08:46 +0900

    マップを生成するクラス

---
+++ SimpleMap2dBuilder.cs
@@ -0,0 +1,90 @@
+namespace TakashiCompany.Unity.Navigator
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	
+
+	public class SimpleMap2dBuilder : MonoBehaviour
+	{
+		[SerializeField]
+		private Vector2Int _grids = new Vector2Int(4, 4);
+
+		[SerializeField]
+		private Vector2 _unitPerGrid = Vector2.one;
+
+		[SerializeField]
+		private LayerMask _groundLayers;
+
+		[SerializeField]
+		private LayerMask _blockLayers;
+
+		private SimpleMap2d _map;
+
+		[ContextMenu("build map")]
+		public SimpleMap2d BuildMap()
+		{
+			_map = BuildMapByOverlapBox(_grids, _unitPerGrid, _groundLayers, _blockLayers);
+
+			return _map;
+		}
+
+		private void OnDrawGizmos()
+		{
+			if (_map == null)
+			{
+				Gizmos.color = Color.red;
+				
+				_grids.Foreach(v2int =>
+				{
+					var p = Utils.GetPositionOnGrid(_grids, v2int, _unitPerGrid).ToV3XZ();
+					Gizmos.DrawWireCube(p, _unitPerGrid.ToV3XZ());
+				});
+			}
+			else
+			{
+				Gizmos.color = Color.red;
+				_grids.Foreach(v2int =>
+				{
+					var reachable = _map.Get(v2int);
+					if (!reachable) return;
+					var p = Utils.GetPositionOnGrid(_grids, v2int, _unitPerGrid).ToV3XZ();
+
+					Gizmos.DrawWireCube(p, _unitPerGrid.ToV3XZ());
+				});
+			}
+		}
+
+		public static SimpleMap2d BuildMapByOverlapBox(Vector2Int grids, Vector2 unitPerGrid, LayerMask groundLayers, LayerMask blockLayers)
+		{
+			var points = new bool[grids.x, grids.y];
+
+			var boxSize = unitPerGrid.ToV3XZ();
+			boxSize.y = 100f;	// 100は適当に設定しました。
+
+			grids.Foreach(v2int =>
+			{
+				var p = Utils.GetPositionOnGrid(grids, v2int, unitPerGrid).ToV3XZ();
+
+
+				var blocks = Physics.OverlapBox(p, boxSize / 2, Quaternion.identity, blockLayers);
+
+				if (blocks != null && blocks.Length > 0)
+				{
+					points[v2int.x, v2int.y] = false;
+					return;
+				}
+
+				var grounds = Physics.OverlapBox(p, boxSize / 2, Quaternion.identity, groundLayers);
+
+				if (blocks != null && blocks.Length > 0)
+				{
+					points[v2int.x, v2int.y] = true;
+					return;
+				}
+			});
+
+			return new SimpleMap2d(points);
+		}
+	}
+}
